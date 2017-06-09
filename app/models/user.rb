015837a962a88dd664192d04ac63837b2a831a02
floodico <floodico@gmail.com>
2017-06-09 20:32:22 +0300

    added following system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :active_followings, class_name:  "Follow", foreign_key: "follower_id", dependent:   :destroy
+  has_many :following, through: :active_followings, source: :followed
+
+  has_many :passive_followings, class_name:  "Follow", foreign_key: "followed_id", dependent:   :destroy
+  has_many :followers, through: :passive_followings, source: :follower
+
+  # Follows a user.
+  def follow(other_user)
+    active_followings.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_followings.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
+
+  resources :users
+
+  post ':id/follow_user', to: 'follows#create', as: :follow_user
+  post ':id/unfollow_user', to: 'follows#destroy', as: :unfollow_user
 
   root 'home#index'
 end
