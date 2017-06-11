202b2446f95e80f86af965104304ec01efcf3d6f
floodico <floodico@gmail.com>
2017-06-12 00:57:04 +0300

    some validations for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,9 @@
 class Song < ApplicationRecord
+  validates :name, :presence => true
   has_attached_file :picture, default_url: "/images/music.png"
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   has_attached_file :audio
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ], message: "has to be in MP3 format"
   validates_attachment_size :audio, :less_than => 10.megabytes
 
   belongs_to :user
