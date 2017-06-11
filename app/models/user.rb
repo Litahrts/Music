bc2e62ad16199e7c0f5553c9ae04c6f0c4e931a2
floodico <floodico@gmail.com>
2017-06-11 03:11:46 +0300

    removed :trackable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :validatable,
          :confirmable
 
   has_one :profile, dependent: :destroy
